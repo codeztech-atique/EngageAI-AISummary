c034563f5677f321d8bbe2c6a7eb81ae4fd84484
Atique Ahmed <atique.ahmed@zoom.us>
2024-10-16 22:59:37 +0530

    Few content added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-be
 const chalk = require('chalk');
 
 
+
+
+
 // Initialize the Bedrock Runtime client
 const client = new BedrockRuntimeClient({ region: "us-east-1" });
 
@@ -32,7 +35,7 @@ async function invokeJambaMini(prompt) {
 
         // console.log("Response:", responseData);
         console.log(chalk.red("\nQuestion Asked:\n"));
-        console.log(chalk.blue(responseData.prompt.text+"\n"))
+        console.log(chalk.blue(prompt+"\n"))
         return responseData;
 
     } catch (error) {
@@ -40,6 +43,51 @@ async function invokeJambaMini(prompt) {
         return null;
     }
 }
+
+const news = `150,000 Gaza children given second polio vaccine dose: WHO, More than 150,000 children in Gaza received the required second dose of oral polio vaccine in the first two days of the campaign's second round, the WHO chief said on Wednesday (Oct 16).
+
+Despite continuing Israeli military operations in some areas of the Palestinian territory, the second round of a polio vaccination campaign, aiming to reach more than 590,000 children under the age of 10, began on Monday.
+
+"The total number of children who received a second dose of polio vaccine in central Gaza after two days of vaccination is 156,943," the World Health Organization's director-general Tedros Adhanom Ghebreyesus said on X.
+
+"The vaccination continues today. At the same time, 128,121 children received vitamin A supplements.
+
+"We call for the humanitarian pauses to continue to be respected. We call for a ceasefire and peace," he said.
+
+As during the initial round of vaccination last month, the second will be divided into three phases, helped by localised "humanitarian pauses" in the fighting: first in central Gaza, then in the south and finally in the hardest-to-reach north of the territory.
+
+Each phase is due to take three campaign days, along with one catch-up day for monitoring and vaccinating any children who were missed.
+
+The vaccination drive began after the Gaza Strip confirmed its first case of polio in 25 years. The disease has re-emerged in besieged Gaza, where the war has left most medical facilities and the sewage system in ruins.
+
+Most often spread through sewage and contaminated water, poliovirus is highly infectious. It can cause deformities and paralysis, and is potentially fatal, mainly affecting children under the age of five.
+
+WHO spokesman Tarik Jasarevic told reporters on Tuesday that nearly 93,000 doses had been administered in central Gaza on Monday's first day, which he said passed "without major issues".
+
+The estimated target for the central area is over 179,000 children under 10.
+
+"We hope that parents will keep coming," Jasarevic said.
+
+Israel launched a military campaign in Gaza after the Oct 7, 2023, attack by Hamas resulted in the deaths of 1,206 people in Israel, mostly civilians, according to an AFP tally of official Israeli figures, including hostages killed in captivity.
+
+`
+
+const blog = `Zoom Meeting SDK for web
+The Zoom Meeting SDK for web lets you embed the Zoom Meeting and Zoom Webinar experiences on a webpage through a highly optimized WebAssembly module. The SDK supports Angular, React, Vue.js, and other JS frameworks.
+
+The SDK strives for feature parity with the Zoom web client, however, some features may not be available. See features supported by web browser for details.
+
+Embed the Meeting SDK
+Embed Zoom on your website using a client view that displays the SDK using the full web page or a component view that splits the client into multiple components that you can position on the page. Or embed the Meeting SDK for web in an iFrame on a webpage and in WebViews on native Android and iOS apps.
+
+Client view
+Client view provides the option to display the Meeting SDK for web as a full page. This allows for a familiar Zoom experience as the client view is similar to the Zoom web client
+, except it lives inside your own web page.
+
+Component view
+The component view provides the option to display the Meeting SDK for web in components on your page. This allows for a more flexible design.
+
+`
 
 const caseDescription = `
 Climate change refers to long-term shifts in temperatures and weather patterns, mainly due to human activities such as burning fossil fuels. 
@@ -60,7 +108,8 @@ Kindly note that if I do not hear back from you by the end of today, I will proc
 Thank you once again for your patience and cooperation. I look forward to hearing from you soon.
 `
 
-invokeJambaMini(case2)
+console.log(chalk.yellow("Please wait..."))
+invokeJambaMini(blog)
     .then(response => {
         console.log(chalk.yellow("\nGenerated Summary with Headlines:\n"));
         console.log(chalk.green(response.completions[0].data.text + "\n"));
